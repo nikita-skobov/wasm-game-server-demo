aa59f483ca0825eb95238bddf74f9fb34b7ee942
nikita-skobov <nskobov.websites@gmail.com>
2023-09-20 18:40:10 -0700

    server queueing message passing

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,16 +1,43 @@
 use macroquad::prelude::*;
 use sapp_jsutils::JsObject;
+use std::sync::{OnceLock, Mutex};
+
+fn ws_messages() -> &'static mut Mutex<Vec<String>> {
+    static mut ARRAY: OnceLock<Mutex<Vec<String>>> = OnceLock::new();
+    unsafe {
+        ARRAY.get_or_init(|| Mutex::new(vec![]));
+        ARRAY.get_mut().unwrap()
+    }
+}
 
 extern "C" {
     fn hi_from_wasm(jsobj: JsObject);
 }
 
 #[no_mangle]
-pub extern "C" fn hi_rust(js_object: JsObject) {
+pub extern "C" fn push_ws_message(jsobj: JsObject) {
     let mut message = String::new();
+    jsobj.to_string(&mut message);
+    if let Ok(mut l) = ws_messages().lock() {
+        l.push(message);
+    } else {
+        miniquad::error!("Failed to get ws lock1!");
+    }
+}
 
-    js_object.to_string(&mut message);
-    miniquad::warn!("printing from rust: {}", message);
+pub fn handle_server_events() {
+    let data = match ws_messages().lock() {
+        Ok(mut items) => {
+            std::mem::take::<Vec<String>>(&mut items.as_mut())
+        }
+        Err(_) => {
+            miniquad::error!("Failed to get ws lock2!");
+            return
+        },
+    };
+    for ev in data {
+        miniquad::warn!("WS: {}", ev);
+    }
 }
 
 #[macroquad::main("BasicShapes")]
@@ -21,6 +48,7 @@ async fn main() {
     }
     loop {
         clear_background(RED);
+        handle_server_events();
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
